5548585aceb3548b5bdca305e93bd48c991c0929
Valentín Peñalver <valenpga@gmail.com>
2024-04-11 19:56:42 +0200

    terminado

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,37 @@
+
+
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { addTask, deleteTask } from './redux/todosSlice';
+
 const App = () => {
-  
+  const tasks = useSelector(state => state.tasks);
+  const dispatch = useDispatch();
+
+  const handleAddTask = () => {
+    const newTask = {
+      id: Math.random().toString(),
+      text: 'Nueva tarea',
+    };
+    dispatch(addTask(newTask));
+  };
+
+  const handleDeleteTask = (taskId) => {
+    dispatch(deleteTask(taskId));
+  };
+
   return (
-    <></>
+    <div>
+      <button onClick={handleAddTask}>Agregar tarea</button>
+      <ul>
+        {tasks.map(task => (
+          <li key={task.id}>
+            {task.text}
+            <button onClick={() => handleDeleteTask(task.id)}>Eliminar</button>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
---
+++ src/slice.js
@@ -0,0 +1,12 @@
+// En todosSlice.js
+
+export const addTask = (task) => ({
+    type: 'ADD_TASK',
+    payload: task,
+  });
+  
+  export const deleteTask = (taskId) => ({
+    type: 'DELETE_TASK',
+    payload: taskId,
+  });
+  
